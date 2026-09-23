73ed85b9501ad8ca42fa32c7c2975d8f57b00f0d
fractalbarista <fractalbarista@gmail.com>
2026-09-23 17:48:48 +0900

    파일 이름을 xxx_spider.py로 변경 API URL을 얻게 된 경위를 추가 custom_settings 삭제

--- musinsa_crawler/spiders/musinsa_product_list_spider.py
+++ musinsa_crawler/spiders/musinsa_product_list_spider.py
@@ -15,6 +15,7 @@ class MusinsaProductListSpider(scrapy.Spider):
     https://api.musinsa.com/api2/dp/v2/plp/goods?gf=M&sortCode=POPULAR&size=60&caller=CATEGORY&page=1&hmacId=1825e8016080aa31f6813eb000ece8009c81d2052350e598da7509934cf551ad&category=001
 
     ## 특이사항
+    - 웹브라우저 개발자도구의 네트워크 탭을 조사하면서 API URL을 얻었습니다. 
     - data.pagination.nextPageUrl을 통해 다음 페이지를 가져올 수 있습니다.
     - size를 예를 들어 200로 크게 설정하면 안됩니다.
     - hmacId가 페이지 마다 다릅니다.
@@ -29,14 +30,6 @@ class MusinsaProductListSpider(scrapy.Spider):
     }
 
     name = "musinsa_product_list_crawler"
-
-    custom_settings = {
-        "USER_AGENT": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/140.0.0.0 Safari/537.36"
-        ),
-    }
 
     async def start(self):
         for category in self.get_categories():
